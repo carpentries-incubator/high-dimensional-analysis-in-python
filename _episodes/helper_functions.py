83f92152238103d209a951ab7c7314111c71f4a6
Chris Endemann <endemann@wisc.edu>
2023-05-22 14:36:42 -0500

    Squashed commit of the following:
    commit f3ea855b7f6ef5b06c8fef74367d885f389f08da
    Author: Chris Endemann <endemann@wisc.edu>
    Date:   Mon May 22 14:33:35 2023 -0500
    
        markdown file for day 1, episode 1
    
    commit d838ea6623739414b17e900fab7a7e295bf4e301
    Author: Chris Endemann <endemann@wisc.edu>
    Date:   Mon May 22 14:32:46 2023 -0500
    
        added script to easily convert between jupyter files and markdown
    
    commit af95bef17616c129cf399dd608db901b83ff5f4f
    Author: ilmcconnell <iain.mcconnell@gmail.com>
    Date:   Mon Apr 17 17:48:58 2023 -0500
    
        EDA tidy up and modelling
    
    commit ead4ebf1e3afd7ba5c211a2f0fc3feb6c3dbdd1f
    Author: ilmcconnell <iain.mcconnell@gmail.com>
    Date:   Mon Apr 17 16:13:29 2023 -0500
    
        day 1 updates
    
    commit 34f4a6794a5869a7418b35d43c8a239ad7cb2d09
    Author: ilmcconnell <iain.mcconnell@gmail.com>
    Date:   Fri Jan 27 16:10:31 2023 -0600
    
        break out some functions to helper function file
    
    commit d9629bcceccf4dd394a1b38338aa1e5d88c20d92
    Author: ilmcconnell <iain.mcconnell@gmail.com>
    Date:   Mon Jul 11 16:43:24 2022 -0500
    
        high dim sphere radius example
    
    commit 4cd578d75f566a9d93a9c3157e23dd887ae707b3
    Author: ilmcconnell <iain.mcconnell@gmail.com>
    Date:   Fri Apr 1 10:01:07 2022 -0500
    
        n-ball intuition

---
+++ _episodes/helper_functions.py
@@ -0,0 +1,199 @@
+import math
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from typing import Optional, Tuple
+
+import numpy as np
+from sklearn.datasets import make_classification
+
+
+def plot_salesprice(df: pd.DataFrame) -> None:
+    # set font size w/ params context manager
+    font = {'font.size': 14}
+    with mpl.rc_context(font):
+        # define values
+        SalePrices = df.SalePrice.tolist()
+        mean = sum(SalePrices)/len(SalePrices)
+        median = sorted(SalePrices)[len(SalePrices)//2]
+
+        # define colors
+        my_blue = [30/255, 136/255, 229/255]
+        my_yellow = [255/255, 193/255, 7/255]
+        my_magenta = [216/255, 27/255, 96/255]
+
+        # plot
+        try:
+            df[~df['top_10']].SalePrice.hist(bins=20, color=my_blue, label='other prices', figsize=(8, 6))
+            df[df['top_10']].SalePrice.hist(bins=20, color='brown', label='Top 10 % of prices', figsize=(8, 6))
+        except (KeyError, ValueError):
+            df.SalePrice.hist(bins=20, color=my_blue, label='prices', figsize=(8, 6))
+        plt.title('SalePrice Distribution: Count vs. SalePrice')
+        plt.ylabel('Count, n')
+        plt.xlabel("Sales Price, USD")
+        plt.xticks(rotation=45, ha='right')
+        plt.axvline(mean, label='mean: {:,.0f}'.format(mean), color=my_magenta)
+        plt.axvline(median, label='median: {:,.0f}'.format(median), color=my_yellow)
+        plt.legend()
+        plt.show()
+        
+
+def split_df(df: pd.DataFrame,
+             split_col: str,
+             bottom_split: Optional[int] = None,
+             top_split: Optional[int] = None) -> pd.DataFrame:
+    """
+    df: df to split (buy rows)
+    split_col: column name with values to split on
+    bottom_split: 2 digit int - percent from bottom for one split
+    top_split: 2 digit int - percent from top for the other split
+    returns: original df with top and bottom split bool columns added.
+    """
+    if bottom_split and top_split:
+        if bottom_split + top_split > 100:
+            raise ValueError('bottom and top splits must not sum to more than 100')
+
+    # top zsplit
+    if top_split:
+        top_split_col_name = f'top_{top_split}'
+        df_top_split = df[split_col].sort_values(ascending=False)[:int(len(df[split_col])/100*top_split)]
+
+        # create new BOOLEAN column that is True when prices equal to or above top 30 value else False
+        df[top_split_col_name] = df[split_col] >= df_top_split.tolist()[-1]
+
+        if not bottom_split:
+            df_bottom_split = df[~df[top_split_col_name]]
+
+    # bottom split
+    if bottom_split:
+        bot_split_col_name = f'bot_{bottom_split}'
+        df_bottom_split = df[split_col].sort_values(ascending=True)[:int(len(df[split_col])/100*bottom_split)]
+
+        # create new BOOLEAN column that is True when prices equal to or below bottom 30 value else False
+        df[bot_split_col_name] = df[split_col] <= df_bottom_split.tolist()[-1]
+
+        if not top_split:
+            df_top_split = df[~df[bot_split_col_name]]
+
+    # sanity warning
+    if bottom_split and top_split:
+        in_both_df = df[df[top_split_col_name] & df[bot_split_col_name]]
+        in_both_df_len = len(in_both_df)
+        if in_both_df_len > 0:
+            logging.warning(f'{in_both_df_len} rows are in both splits:\n{in_both_df}')
+
+    return df
+
+
+def demo_standardization(column_name: str, df: pd.DataFrame) -> None:
+    raw = df[column_name].tolist()
+    mean = sum(raw)/len(raw)
+    variances = [(x-mean)**2 for x in raw]
+    variance = sum(variances)/len(variances)
+    stdev = math.sqrt(variance)
+    standardized = [(x-mean)/stdev for x in raw]
+
+    standardized_mean = sum(standardized)/len(standardized)
+    s_variances = [(x-standardized_mean)**2 for x in standardized]
+    s_variance = sum(s_variances)/len(s_variances)
+    standardized_stdev = math.sqrt(s_variance)
+
+    fig, ax = plt.subplots(1, 1, figsize=(8,8))
+    
+    (raw_hts, raw_edges, raw_plot) = ax.hist(
+        raw, 
+        label='raw', 
+        alpha=0.6
+    )
+    x_min = min(raw_edges)
+    x_max = max(raw_edges)
+    y_min = 0
+    y_max = max(raw_hts)
+    x_range = x_max - x_min
+    y_range = y_max
+    plt.title('raw')
+    ax.set_ylabel('number of observations')
+    ax.set_xlabel(column_name)
+    plt.text(x_min + (x_range/10)*7.5, y_min + y_range/10*9, 'raw mean: {:.2f}'.format(mean))
+    plt.text(x_min + (x_range/10)*7.5, y_min + y_range/10*8, 'raw stdev {:.2f}'.format(stdev))
+
+
+    (std_hts, std_edges, std_plot) = ax.hist(
+        standardized, 
+        label='standardized', 
+        alpha=0.6
+    )
+    x_min = min(std_edges)
+    x_max = max(std_edges)
+    y_min = 0
+    y_max = max(std_hts)
+    x_range = x_max - x_min
+    y_range = y_max
+    plt.text(x_min + (x_range/10)*7.5, y_min + y_range/10*9, 'std mean {:.2f}'.format(standardized_mean))
+    plt.text(x_min + (x_range/10)*7.5, y_min + y_range/10*8, 'std stdev {:.2f}'.format(standardized_stdev))
+    ax.legend()
+
+    plt.show()
+    
+
+def create_feature_data() -> np.ndarray:
+    '''create x and y correlated variable'''
+    X1, Y1 = make_classification(
+        n_features=2, n_redundant=0, n_informative=2, n_clusters_per_class=1
+    )
+
+    feature_1 = [x for x, y in zip(X1, Y1) if y == 0]
+    return np.stack(feature_1)
+
+
+def create_normalized_feature(arr: np.ndarray) -> np.ndarray:
+    """center data at 0"""
+    normalized_arr = np.zeros_like(arr)
+    for dim in range(arr.shape[-1]):
+        data = arr[:, dim]
+        normalized_arr[:, dim] = (data-np.mean(data))/np.std(data)
+    return normalized_arr
+
+
+def create_feature_scatter_plot() -> Tuple[np.ndarray, Tuple[float, float]]:
+    # feature_data = create_feature_data()
+    feature_data = create_normalized_feature(create_feature_data())
+    fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+    im = ax.scatter(feature_data[:,0], feature_data[:,1], label='original features')
+    # ensure square plot
+    xlims = ax.get_xlim()
+    ylims = ax.get_ylim()
+    min_ax_val = min(xlims[0], ylims[0])
+    max_ax_val = max(xlims[1], ylims[1])
+    ax.set_xlim(min_ax_val, max_ax_val)
+    ax.set_ylim(min_ax_val, max_ax_val)
+    # label and plot
+    ax.set_title('random feature data scatter plot')
+    ax.set_xlabel('feature x value, arbitrary units')
+    ax.set_ylabel('feature y value, arbitrary units')
+    return ax, feature_data, (min_ax_val, max_ax_val)
+
+
+def plot_pca_features(features_pca: np.ndarray) -> Tuple[plt.Figure, plt.Axes, Tuple[float, float]]:
+    """
+    plot PCA data centreed in a square plot
+    """
+    # get extents, set to extremes
+    min_x = min(features_pca[:,0])
+    max_x = max(features_pca[:,0])
+    min_y = min(features_pca[:,1])
+    max_y = max(features_pca[:,1])
+    ax_max = max(max_x, max_y)
+    ax_min = min(min_x, min_y)
+
+    # plot data within extents above
+    fig, pca_ax = plt.subplots(1,1,figsize=(8, 8))
+    pca_im = pca_ax.scatter(features_pca[:,0], features_pca[:, 1], color='orange')
+    pca_ax.set_xlim(ax_min, ax_max)
+    pca_ax.set_ylim(ax_min, ax_max)
+    pca_ax.set_xlabel('pca component 0')
+    pca_ax.set_ylabel('pca component 1')
+    pca_ax.set_title('2 component pca')
+    return fig, pca_ax, (ax_min, ax_max)
+
+
